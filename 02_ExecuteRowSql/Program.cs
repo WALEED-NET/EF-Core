891de8da7add0410cc1ab758573935d120e014d5
Waleed Bensumaidea <waleed.com145@gmail.com>
2023-08-31 17:21:43 +0300

    Add 02_ExecuteRowSql_Project

---
+++ 02_ExecuteRowSql/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace _02ConnectionString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Console.WriteLine(configuration.GetSection("constr").Value);
+            Console.ReadKey();
+        }
+    }
+}
