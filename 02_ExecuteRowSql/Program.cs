c34d288ca22628f40dd76da50b52bfea4190e8f9
Waleed Bensumaidea <waleed.com145@gmail.com>
2023-08-31 20:58:11 +0300

    Learn Execute Row Sql Select Statement

--- 02_ExecuteRowSql/Program.cs
+++ 02_ExecuteRowSql/Program.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Globalization;
 
 namespace _02ConnectionString
 {
+
     internal class Program
     {
         static void Main(string[] args)
@@ -10,7 +14,31 @@ namespace _02ConnectionString
                 .AddJsonFile("appsettings.json")
                 .Build();
 
-            Console.WriteLine(configuration.GetSection("constr").Value);
+            SqlConnection conn = new SqlConnection(configuration.GetSection("constr").Value); //1 
+
+            var sqlQuery = "Select * From Wallets "; //2
+
+            SqlCommand command = new SqlCommand(sqlQuery , conn); // 3
+            command.CommandType = CommandType.Text;// 4
+
+            conn.Open();    // 5
+
+            SqlDataReader reader = command.ExecuteReader(); //6
+
+            Wallet wallet;
+            while (reader.Read())   // 7 how to get data into object
+            {
+                wallet = new Wallet()
+                {
+                    Id = reader.GetInt32("Id"),
+                    Holder = reader.GetString("Holder"),
+                    Balance = reader.GetDecimal("Balance")
+                };
+                Console.WriteLine(wallet);
+            }
+
+            conn.Close();
+            
             Console.ReadKey();
         }
     }
