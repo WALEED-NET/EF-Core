25728dc28efa40211db15d91e76f9339732e4f81
Waleed Bensumaidea <waleed.com145@gmail.com>
2023-08-31 16:38:45 +0300

    Add project files.

---
+++ 02ConnectionString/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02ConnectionString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
